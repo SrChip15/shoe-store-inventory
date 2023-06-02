228ae5935200808ef1aed313e532f041be463f4c
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-06-02 16:13:19 -0400

    nav from instructions to shoe list

--- app/src/main/java/com/udacity/shoestore/screens/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/instructions/InstructionsFragment.kt
@@ -2,14 +2,20 @@ package com.udacity.shoestore.screens.instructions
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
-class InstructionsFragment: Fragment() {
+@Suppress("DEPRECATION", "OVERRIDE_DEPRECATION")
+class InstructionsFragment : Fragment() {
     private lateinit var binding: FragmentInstructionsBinding
 
     override fun onCreateView(
@@ -17,8 +23,21 @@ class InstructionsFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+        setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.instructions_overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            requireView().findNavController()
+        ) || super.onOptionsItemSelected(item)
     }
 }
