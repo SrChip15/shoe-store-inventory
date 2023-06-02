2f0626f455202303044e894ecfe5104cee9a9ced
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-06-02 16:12:13 -0400

    add shoe list fragment

---
+++ app/src/main/java/com/udacity/shoestore/screens/shoes/ShoesFragment.kt
@@ -0,0 +1,31 @@
+package com.udacity.shoestore.screens.shoes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentShoesBinding
+
+class ShoesFragment: Fragment() {
+
+    private lateinit var binding: FragmentShoesBinding
+    private lateinit var viewModel: ShoesViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoes, container, false)
+
+        viewModel = ViewModelProvider(this)[ShoesViewModel::class.java]
+        binding.shoesViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+}
