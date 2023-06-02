1ed52c1b1e24d4c7cf979ba554bfaeb32fab6032
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-06-02 08:28:44 -0400

    enable menu item display

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
 
         Timber.plant(Timber.DebugTree())
     }
