fe6f38ca68dcfc134e5872c61fdca9f9cc453083
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-06-05 11:47:12 -0400

    add nav to shoe detail screen

--- app/src/main/java/com/udacity/shoestore/screens/shoes/ShoesFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoes/ShoesFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoesBinding
 
@@ -25,6 +26,13 @@ class ShoesFragment: Fragment() {
         viewModel = ViewModelProvider(this)[ShoesViewModel::class.java]
         binding.shoesViewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(
+                ShoesFragmentDirections.actionShoesFragmentToShoeDetailFragment()
+
+            )
+        }
 
         return binding.root
     }
